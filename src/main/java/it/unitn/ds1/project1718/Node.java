a3c0a6362ebe02fb779a65293f2eb8deef069cc5
andreazampieri <andreazampieri95@gmail.com>
2018-05-22 14:51:42 +0200

    added multicast to Node.java

--- src/main/java/it/unitn/ds1/project1718/Node.java
+++ src/main/java/it/unitn/ds1/project1718/Node.java
@@ -6,10 +6,14 @@ import it.unitn.ds1.project1718.Messages.StartMessage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
+import java.util.Random;
 
 public abstract class Node extends AbstractActor {
     protected int id;
     protected List<ActorRef> participants;
+
+    protected rnd = new Random();
 
     public Node(int id) {
         super();
@@ -24,4 +28,20 @@ public abstract class Node extends AbstractActor {
             }
         }
     }
+
+    private void multicast(Serializable m) {
+        List<ActorRef> shuffledGroup = new ArrayList<>(participants);
+        Collections.shuffle(shuffledGroup);
+        for(ActorRef p:shuffledGroup){
+            if(!p.equals(getSelf())) {
+                p.tell(m,getSelf());
+                try {
+                    Thread.sleep(rnd.nextInt(10));
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
